424c7520af932ac89e5e2c7ee9f425574bc202e0
bit-modulator <neurapraxia@gmail.com>
2021-05-11 15:42:42 +0100

    working on styling yet again :(

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 
 - Initially logic created - fetching and displaying the recipes
 - Styling - mobile first web development - starting with a narrow viewport (tested using chrome device simulator)
-- 
+-   I implemented one event listener for the whole list, rather than one 
+  per element as I didn't want potentially hundreds of them. 
 
 
--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const RECIPE_APP_ID = "ac929366"
 const RECIPE_APP_KEY = "c9a3f34b5eec7c0fbeae3ab5f919746e"
-const authKey = `&app_id=${RECIPE_APP_ID}&app_key=${RECIPE_APP_KEY}`
+const AUTH_KEY = `&app_id=${RECIPE_APP_ID}&app_key=${RECIPE_APP_KEY}`
+const URI_LENGTH = 32;
 
+//DOM--#################################################################
 const foodItemSearchBtn = document.querySelector("#recipe-button");
 const itemInputElem = document.querySelector("#food-input");
 const recipeResultsElem = document.querySelector(".recipe-results");
 const recipeResultsNumElem = document.querySelector("#recipe-result-numbers")
 const htmlElem = document.querySelector("html");
 
+//Event Listeners--#####################################################
 foodItemSearchBtn.addEventListener("click", handleRecipeClick);
 window.onscroll = function(event){
   //If we have scrolled to the last visible result, fetch some more
@@ -15,9 +18,14 @@ window.onscroll = function(event){
     getMoreRecipes(moreRecipes);
   };
 };
+
+//Global Variables--####################################################
+
 let numRecipesToDisplay = 10; //limit I have placed due to avoid exceeding free API limit and reduce latency
 let moreRecipes;
 const recipeDict = {};
+
+//Functions--##########################################################
 
 function handleRecipeClick() {
   /*#################################################################
@@ -28,7 +36,6 @@ function handleRecipeClick() {
   }
   const foodToSearch = itemInputElem.value;
   if (foodToSearch){
-    console.log("Searching...");
     fetchRecipe(foodToSearch);
   } else{
     console.error("No search item entered. Please enter an item and try again!");
@@ -44,13 +51,11 @@ async function fetchRecipe(food, from=0) {
   #################################################################*/
   try{
     const fromQuery = `&from=${from}`;
-    const requestUrl = `https://api.edamam.com/search?q=${food+authKey+fromQuery}`
+    const requestUrl = `https://api.edamam.com/search?q=${food+AUTH_KEY+fromQuery}`
     const recipeSearchResponse = await fetch(requestUrl, {cache: "force-cache"}); //change this to default after testing completed
     const recipeList = await recipeSearchResponse.json();
-    console.log(recipeList);//----------------Remove this later: for debugging only---------------------
     moreRecipes = recipeList.more;
     recipeList.hits.forEach(recipe => recipeDict[recipe.recipe.uri.substr(recipe.recipe.uri.lastIndexOf("_")+1)] = recipe.recipe);
-    console.log(recipeDict)
     displayRecipeCount(recipeList.count); //?bug - returns one less than is available sometimes
     //displayRecipeCount(recipeList.hits.length);
     displayRecipeSearchResults(recipeList.hits, moreRecipes);
@@ -89,40 +94,61 @@ function displayRecipeSearchResults(recipeList, moreRecipes){
     olElem = document.createElement("ol");
     olElem.classList.add("recipe-list");
     recipeResultsElem.appendChild(olElem);
+    olElem.addEventListener("click", function(event){
+      displayDetails(event.target);
+    });
   };
   recipeList.forEach((recipe,index) => {
     const liElem = document.createElement("li");
     liElem.classList.add("recipe-item")
-    liElem.innerHTML= formatRecipeResults(recipe.recipe);
     let uri = recipe.recipe.uri.substr(recipe.recipe.uri.lastIndexOf("_")+1);
-    liElem.id = uri;
+    liElem.innerHTML= formatRecipeResults(recipe.recipe, uri);
+    liElem.classList.add(uri);
     olElem.appendChild(liElem);
   });
   if ((olElem.getBoundingClientRect().bottom <= window.innerHeight) && moreRecipes){
     getMoreRecipes(moreRecipes);
   }
- 
+
 };
 
-function formatRecipeResults(recipe){
+function formatRecipeResults(recipe, uri){
   /*#################################################################
   wrap img in an anchor tag, insert text +/- calories, prep time, labels
   #################################################################*/
   let totTime = recipe.totalTime;
   let prepTime = (totTime > 0)? (totTime <= 60)? `Time: ${totTime}mins`:`Time: ${Math.round(totTime/60)}hrs`: "";
   const recipeCard = `
-  <img class="recipe-image" src="${recipe.image}" alt="${recipe.label}"">
-  <p id="info">${recipe.label}.<br>Serves: ${recipe.yield}.<br>${prepTime}</p>`
+  <img class="${uri}" src="${recipe.image}" alt="${recipe.label}"">
+  <p id="info" class="recipe-details ${uri}">${recipe.label}.<br>Serves: ${recipe.yield}.<br>${prepTime}</p>`
   return recipeCard;
 }
 
 function getMoreRecipes(moreRecipes){
+  /*#################################################################
+  fetch more recipes if they reach the bottom of the page
+  #################################################################*/
   if (moreRecipes){
     fetchRecipe(itemInputElem.value, from=numRecipesToDisplay);
-    console.log(`numRecipesToDisplay: ${numRecipesToDisplay}`);
     numRecipesToDisplay += 11;
   }
 }
-{/* <a href=${recipe.url}></a> */}
+/* <a href=${recipe.url}></a> */
 
-//http://www.edamam.com/ontologies/edamam.owl#recipe_
+function displayDetails(item){
+  /*#################################################################
+  Get the recipe ID of the clicked on element, if they clicked on the space
+  #################################################################*/
+  let recipeClicked = item.className.substr(item.className.lastIndexOf(" ")+1);
+  if (item.parentNode.nodeName != "LI"){
+    return; //do nothing if the space between the recipe cards was clicked
+  }
+  console.log(item.parentNode)
+  if (item.parentNode.classList.contains("detailed-recipe")){
+    item.parentNode.classList.remove("detailed-recipe");
+  } else{
+    item.parentNode.classList.add("detailed-recipe");
+  }
+  console.log(recipeDict[recipeClicked]);
+  // fetchLargerImage()
+}
